266fd08893a5df9c92dc2007603e069edcf4382f
Artsiom Sasnouski <asasnouski@exadel.com>
2015-11-06 17:15:15 +0300

    Refactored controller, added service and history

--- js/controllers/workout-controller.js
+++ js/controllers/workout-controller.js
@@ -1,8 +1,6 @@
 'use strict';
 
-angular.module('7minute').controller('WorkoutController', ['$scope','$interval', '$location', function($scope, $interval, $location) {
-    var restExercise,
-        exerciseIntervalPromise;
+angular.module('7minute').controller('WorkoutController', ['$scope','$interval', '$location', 'workoutHistoryTracker', function($scope, $interval, $location, workoutHistoryTracker) {
     function WorkoutPlan(args) {
         this.exercises = [];
         this.name = args.name;
@@ -17,6 +15,7 @@ angular.module('7minute').controller('WorkoutController', ['$scope','$interval',
             return this.restBetweenExercise * (this.exercises.length - 1) + total;
         }
     };
+
     function Exercise(args) {
         this.name = args.name;
         this.title = args.title;
@@ -27,6 +26,9 @@ angular.module('7minute').controller('WorkoutController', ['$scope','$interval',
         this.nameSound = args.nameSound;
         this.procedure = args.procedure;
     }
+
+    var restExercise;
+    var exerciseIntervalPromise;
     var startWorkout = function () {
         $scope.workoutPlan = createWorkout();
         $scope.workoutTimeRemaining = $scope.workoutPlan.totalWorkoutDuration();
@@ -39,10 +41,7 @@ angular.module('7minute').controller('WorkoutController', ['$scope','$interval',
             }),
             duration: $scope.workoutPlan.restBetweenExercise
         };
-        $interval(function () {
-            $scope.workoutTimeRemaining = $scope.workoutTimeRemaining - 1;
-        }, 1000, $scope.workoutTimeRemaining);
-
+        workoutHistoryTracker.startTracking();
         $scope.currentExerciseIndex = -1;
         startExercise($scope.workoutPlan.exercises[0]);
     };
@@ -55,19 +54,9 @@ angular.module('7minute').controller('WorkoutController', ['$scope','$interval',
             $scope.currentExerciseIndex++;
         }
 
-        $interval(function () {
-            ++$scope.currentExerciseDuration;
-        }, 1000, $scope.currentExercise.duration)
-            .then(function () {
-                var next = getNextExercise(exercisePlan);
-                if (next) {
-                    startExercise(next);
-                }
-                else {
-                    $location.path('/finish');
-                }
-            });
+        exerciseIntervalPromise = startExerciseTimeTracking();
     };
+
     var getNextExercise = function (currentExercisePlan) {
         var nextExercise = null;
         if (currentExercisePlan === restExercise) {
@@ -80,90 +69,118 @@ angular.module('7minute').controller('WorkoutController', ['$scope','$interval',
         }
         return nextExercise;
     };
-    var startExercise = function (exercisePlan) {
-        $scope.currentExercise = exercisePlan;
-        $scope.currentExerciseDuration = 0;
-        exerciseIntervalPromise = startExerciseTimeTracking();
-    };
-    var startExerciseTimeTracking = function () {
-        var promise = $interval(function () {
-            ++$scope.currentExerciseDuration;
-            --$scope.workoutTimeRemaining;
-        }, 1000, $scope.currentExercise.duration
-            - $scope.currentExerciseDuration);
-        promise.then(function () {
-            var next = getNextExercise($scope.currentExercise);
-            if (next) {
-                startExercise(next);
-            } else {
-                $location.path('/finish');
-            }});
-        return promise;
-    };
+
     $scope.pauseWorkout = function () {
         $interval.cancel(exerciseIntervalPromise);
         $scope.workoutPaused = true;
     };
+
     $scope.resumeWorkout = function () {
         exerciseIntervalPromise = startExerciseTimeTracking();
         $scope.workoutPaused = false;
     };
+
     $scope.pauseResumeToggle = function () {
         if ($scope.workoutPaused) {
             $scope.resumeWorkout();
-        } else {
+        }
+        else {
             $scope.pauseWorkout();
         }
     }
+
+    var startExerciseTimeTracking = function () {
+        var promise = $interval(function () {
+            ++$scope.currentExerciseDuration;
+            --$scope.workoutTimeRemaining;
+        }, 1000, $scope.currentExercise.duration - $scope.currentExerciseDuration);
+
+        promise.then(function () {
+            var next = getNextExercise($scope.currentExercise);
+            if (next) {
+                startExercise(next);
+            }
+            else {
+                workoutComplete();
+            }
+        }, function (error) {
+            console.log('Inteval promise cancelled. Error reason -' + error);
+        });
+        return promise;
+    }
+
+    var workoutComplete = function () {
+        workoutHistoryTracker.endTracking(true);
+        $location.path('/finish');
+    }
+
+    $scope.onKeyPressed = function (event) {
+        if (event.which == 80 || event.which == 112) {        // 'p' or 'P' key to toggle pause and resume.
+            $scope.pauseResumeToggle();
+        }
+    };
+
+    //$scope.$watch('currentExerciseDuration', function (nVal) {
+    //    if (nVal == $scope.currentExercise.duration) {
+    //        var next = getNextExercise($scope.currentExercise);
+    //        if (next) {
+    //            startExercise(next);
+    //        } else {
+    //            console.log("Workout complete!")
+    //        }
+    //    }
+    //});
+
     var createWorkout = function () {
         var workout = new WorkoutPlan({
             name: "7minWorkout",
             title: "7 Minute Workout",
-            restBetweenExercise: 5
+            restBetweenExercise: 10
         });
-    workout.exercises.push({
-        details: new Exercise({
-            name: "jumpingJacks",
-            title: "Jumping Jacks",
-            description: "A jumping jack or star jump, also called side-straddle hop is a physical jumping exercise.",
-            image: "img/JumpingJacks.png",
-            nameSound: "content/jumpingjacks.wav",
-            videos: ["//www.youtube.com/embed/dmYwZH_BNd0", "//www.youtube.com/embed/BABOdJ-2Z6o", "//www.youtube.com/embed/c4DAnQ6DtF8"],
-            procedure: "Assume an erect position, with feet together and arms at your side.\
-                        <br/>Slightly bend your knees, and propel yourself a few inches into the air.\
-                        <br/>While in air, bring your legs out to the side about shoulder width or slightly wider.\
-                        <br/>As you are moving your legs outward, you should raise your arms up over your head; arms should be slightly bent throughout the entire in-air movement.\
-                        <br/>Your feet should land shoulder width or wider as your hands meet above your head with arms slightly bent"
-        }),
-        duration: 30
-    });
-    workout.exercises.push({
-        details: new Exercise({
-            name: "wallSit",
-            title: "Wall Sit",
-            description: "A wall sit, also known as a Roman Chair, is an exercise done to strengthen the quadriceps muscles.",
-            image: "img/wallsit.png",
-            nameSound: "content/wallsit.wav",
-            videos: ["//www.youtube.com/embed/y-wV4Venusw", "//www.youtube.com/embed/MMV3v4ap4ro"],
-            procedure: "Place your back against a wall with your feet shoulder width apart and a little ways out from the wall.\
-                        <br/>Then, keeping your back against the wall, lower your hips until your knees form right angles. "
-        }),
-        duration: 30
-    });
-    workout.exercises.push({
-        details: new Exercise({
-            name: "pushUp",
-            title: "Push Up",
-            description: "A push-up is a common exercise performed in a prone position by raising and lowering the body using the arms",
-            image: "img/Pushup.png",
-            nameSound: "content/pushups.wav",
-            videos: ["//www.youtube.com/embed/Eh00_rniF8E", "//www.youtube.com/embed/ZWdBqFLNljc", "//www.youtube.com/embed/UwRLWMcOdwI", "//www.youtube.com/embed/ynPwl6qyUNM", "//www.youtube.com/embed/OicNTT2xzMI"],
-            procedure: "Lie prone on the ground with hands placed as wide or slightly wider than shoulder width. \
-                        <br/>Keeping the body straight, lower body to the ground by bending arms at the elbows. \
-                        <br/>Raise body up off the ground by extending the arms."
-        }),
-        duration: 30
-    });
+
+        workout.exercises.push({
+            details: new Exercise({
+                name: "jumpingJacks",
+                title: "Jumping Jacks",
+                description: "A jumping jack or star jump, also called side-straddle hop is a physical jumping exercise.",
+                image: "img/JumpingJacks.png",
+                nameSound: "content/jumpingjacks.wav",
+                videos: ["dmYwZH_BNd0", "BABOdJ-2Z6o", "c4DAnQ6DtF8"],
+                procedure: "Assume an erect position, with feet together and arms at your side.\
+                            <br/>Slightly bend your knees, and propel yourself a few inches into the air.\
+                            <br/>While in air, bring your legs out to the side about shoulder width or slightly wider.\
+                            <br/>As you are moving your legs outward, you should raise your arms up over your head; arms should be slightly bent throughout the entire in-air movement.\
+                            <br/>Your feet should land shoulder width or wider as your hands meet above your head with arms slightly bent"
+            }),
+            duration: 30
+        });
+        workout.exercises.push({
+            details: new Exercise({
+                name: "wallSit",
+                title: "Wall Sit",
+                description: "A wall sit, also known as a Roman Chair, is an exercise done to strengthen the quadriceps muscles.",
+                image: "img/wallsit.png",
+                nameSound: "content/wallsit.wav",
+                videos: ["y-wV4Venusw", "MMV3v4ap4ro"],
+                procedure: "Place your back against a wall with your feet shoulder width apart and a little ways out from the wall.\
+                              <br/>Then, keeping your back against the wall, lower your hips until your knees form right angles. "
+            }),
+            duration: 30
+        });
+        workout.exercises.push({
+            details: new Exercise({
+                name: "pushUp",
+                title: "Push Up",
+                description: "A push-up is a common exercise performed in a prone position by raising and lowering the body using the arms",
+                image: "img/Pushup.png",
+                nameSound: "content/pushups.wav",
+                videos: ["Eh00_rniF8E", "ZWdBqFLNljc", "UwRLWMcOdwI", "ynPwl6qyUNM", "OicNTT2xzMI"],
+                procedure: "Lie prone on the ground with hands placed as wide or slightly wider than shoulder width. \
+                              Keeping the body straight, lower body to the ground by bending arms at the elbows. \
+                              Raise body up off the ground by extending the arms."
+            }),
+            duration: 30
+        });
     return workout;
     };
 
---
+++ js/services/services.js
@@ -0,0 +1,32 @@
+angular.module('7minute')
+    .factory('workoutHistoryTracker', ['$rootScope', function
+        ($rootScope) {
+        var maxHistoryItems = 20; //Track for last 20 exercise
+        var workoutHistory = [];
+        var currentWorkoutLog = null;
+        var service = {};
+        service.startTracking = function () {
+            currentWorkoutLog = { startedOn: new Date().toISOString(),
+                completed: false,
+                exercisesDone: 0 };
+            if (workoutHistory.length >= maxHistoryItems) {
+                workoutHistory.shift();
+            }
+            workoutHistory.push(currentWorkoutLog);
+        };
+        service.endTracking = function (completed) {
+            currentWorkoutLog.completed = completed;
+            currentWorkoutLog.endedOn = new Date().toISOString();
+            currentWorkoutLog = null;
+        };
+        service.getHistory = function () {
+            return workoutHistory;
+        };
+        $rootScope.$on("$routeChangeSuccess", function (e, args) {
+            if (currentWorkoutLog) {
+                service.endTracking(false);
+            }
+        });
+        return service;
+    }]);
+
